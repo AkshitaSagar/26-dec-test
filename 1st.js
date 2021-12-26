d4ee8d987659004fc592eb9961d9d0ef81e8d06d
AkshitaSagar <94096782+AkshitaSagar@users.noreply.github.com>
2021-12-26 17:48:50 +0530

    Add files via upload

---
+++ 1st.js
@@ -0,0 +1,10 @@
+//console.log("hii");
+/*Write a program to find nth number of arithmetic progression(AP). 
+Push your code on Github and share that link here. */
+
+var a = 7;
+var d= 3;
+n=prompt('enter number series');
+n=parseInt(n);
+output=(a+(n-1)*d);
+console.log(output);
